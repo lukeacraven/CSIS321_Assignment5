9d289a8b4be44f38e40700b19941be745af051be
lukeacraven <lcraven@2UA4241N37.University.liberty.edu>
2018-02-20 09:43:08 -0500

    Started work on CharacterLinkedList

--- CSIS321_Assignment5/CharacterLinkedList.java
+++ CSIS321_Assignment5/CharacterLinkedList.java
@@ -1,11 +1,27 @@
-
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
 
 public class CharacterLinkedList
 {
 
 	public static void main(String[] args)
 	{
+		Random rand = new Random();
+		rand.setSeed(System.currentTimeMillis());
 		
+		List<Character> list = new ArrayList<Character>();
+		
+		for(int i = 0; i < 10; i++)
+			list.add((char)(rand.nextInt(26) + 'a'));
+		
+		System.out.println("List order:");
+		System.out.println(Arrays.toString(list.toArray()));
+		
+		System.out.println("\nReverse list order:");
+		System.out.println(Arrays.toString(list.toArray()));
 	}
 
 }
